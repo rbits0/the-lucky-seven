538a5bd6294a57d18b6384181d4e8fcfadc58e6a
rbits0 <rbits0@gmail.com>
2023-11-07 23:31:05 +1030

    Add dealEnemies() function

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Cell from "./Cell";
-import { CardData, CardType, PlayerCard } from "./CardData";
+import { CardData, CardType, EnemyCard, PlayerCard } from "./CardData";
 import { DndContext, DragEndEvent, DragStartEvent } from "@dnd-kit/core";
 import Card from "./Card";
 
@@ -10,9 +10,74 @@ const NUM_COLUMNS = 6
 
 
 function Grid() {
-  const [list, setList] = useState<(CardData | null)[][]>(createRandomBoard());
-  const [deck, setDeck] = useState<CardData[]>([]);
+  const [initialBoard, initialDeck] = createRandomBoard();
+  const [list, setList] = useState<(CardData | null)[][]>(initialBoard);
+  const [deck, setDeck] = useState<EnemyCard[]>(initialDeck);
   
+
+  function dealEnemies() {
+    if (deck.length < NUM_ROWS) {
+      return
+    }
+
+    const newDeck = [...deck];
+    const newList = list.map(row => [...row]);
+
+    for (let row = 0; row < NUM_ROWS; row++) {
+      // Deal top card
+      const card = newDeck.pop()!;
+      
+
+      // Work out position of card
+      let position = card.position;
+      // If it can't overlap, need to find position where it doesn't overlap
+      if (!card.canOverlap && newList[row][position]) {
+        // Determine whether to prioritise left or right
+        const prioritisedDirection = position < NUM_COLUMNS / 2 ? -1 : 1;
+        
+        let index = 1;
+        let prioritisedExhausted = false;
+        let unprioritisedExhausted = false;
+
+        while (!(prioritisedExhausted && unprioritisedExhausted)) {
+          // Try prioritised direction first
+          let attemptPosition = position + index * prioritisedDirection;
+          if (attemptPosition < 0 || attemptPosition >= NUM_COLUMNS) {
+            prioritisedExhausted = true;
+          } else {
+            if (!newList[row][attemptPosition]) {
+              // Success
+              position = attemptPosition;
+              break;
+            }
+          }
+          
+          // Try other direction
+          attemptPosition = position + index * (prioritisedDirection * -1);
+          if (attemptPosition < 0 || attemptPosition >= NUM_COLUMNS) {
+            unprioritisedExhausted = true;
+          } else {
+            if (!newList[row][attemptPosition]) {
+              // Success
+              position = attemptPosition;
+              break;
+            }
+          }
+          
+          // Increment index
+          index += 1;
+        }
+      }
+      
+
+      // Place card
+      newList[row][position] = card;
+    }
+
+    // Save new deck and list
+    setList(newList);
+    setDeck(newDeck);
+  }
 
   useEffect(() => {
   }, [])
@@ -80,7 +145,7 @@ function Grid() {
 }
 
 
-function createRandomBoard(): (CardData | null)[][] {
+function createRandomBoard(): [(CardData | null)[][], EnemyCard[]] {
     const cards = require("./cards.json");
     const list = [...Array(NUM_ROWS)].map(() => Array(NUM_COLUMNS).fill(null));
     
@@ -88,6 +153,7 @@ function createRandomBoard(): (CardData | null)[][] {
     let row_numbers: number[][] = shuffleArray(cards.rows);
     let column_numbers: number[][] = shuffleArray(cards.columns);
     
+
     // Shuffle player cards
     let players = shuffleArray(
       (cards.players as any[]).map((card, i) => (
@@ -108,9 +174,23 @@ function createRandomBoard(): (CardData | null)[][] {
       console.log(`${row_index}, ${column_index}`)
       list[row_index][column_index] = player;
     });
+    
+
+    // Parse enemy cards
+    let enemies: EnemyCard[] = [];
+    for (const [i, enemy] of cards.enemies.entries()) {
+      for (const position of enemy.positions) {
+        const newEnemy = { ...enemy, position: position, id: `enemy${i}`, type: CardType.Enemy};
+        delete newEnemy.positions;
+        enemies.push(newEnemy);
+      }
+    }
+    
+    // Shuffle enemy cards
+    shuffleArray(enemies);
 
     
-    return list;
+    return [list, enemies];
 }
 
 
