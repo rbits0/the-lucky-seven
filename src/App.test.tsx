5aeeea8d78a058cb2fa6ed7a8635c1b9f0324be7
rbits0 <rbits0@gmail.com>
2024-10-10 11:38:36 +1100

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,32 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders grid', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const gridElement = screen.getByRole('table');
+  expect(gridElement).toBeInTheDocument();
+});
+
+test('renders phase', () => {
+  render(<App />);
+  const phaseElement = screen.getByText(/Phase: Game Start/i);
+  expect(phaseElement).toBeInTheDocument();
+});
+
+test('renders flip selected button', () => {
+  render(<App />);
+  const flipElement = screen.getByText(/Flip Selected/i);
+  expect(flipElement).toBeInTheDocument();
+});
+
+test('renders next phase button', () => {
+  render(<App />);
+  const nextPhaseElement = screen.getByText(/Next Phase/i);
+  expect(nextPhaseElement).toBeInTheDocument();
+});
+
+test('renders undo button', () => {
+  render(<App />);
+  const undoElement = screen.getByText(/Undo/i);
+  expect(undoElement).toBeInTheDocument();
 });
